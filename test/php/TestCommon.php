d4311bd3b23e6a1b92489f6079797010e4a2ae62
Igor Prochazka <igor.prochazka@gmail.com>
2011-12-04 21:47:38 +0100

    Move common test code to TestCommon.php

---
+++ test/php/TestCommon.php
@@ -0,0 +1,21 @@
+<?php
+
+define('DEFDIR', dirname(__FILE__) . '/../def/');
+
+class TestCommon extends PHPUnit_Framework_TestCase {
+		function test_all() {
+		$lines = file( DEFDIR . $this->infile);
+		if($lines === false) {
+			$this->assertTrue(false, 'Can\t load test definition file');
+		}
+		foreach($lines as $line) {
+			if($line[0] == '#')
+				continue;
+			$arr = str_getcsv($line, '|', "'");
+			if(count($arr)!=3) {
+				$this->assertTrue(false, 'Expected 3 columns in definition file');
+			}
+			$this->check(trim($arr[0]), trim($arr[1]), trim($arr[2]));
+		}
+	}
+}
--- test/php/TokenizerTest.php
+++ test/php/TokenizerTest.php
@@ -1,15 +1,16 @@
 <?php
 
 require_once('../../jazzy-forms/core/Tokenizer.php');
+require_once('TestCommon.php');
 
 /**
  * @backupGlobals disabled
  */
-class TokenizerTest extends PHPUnit_Framework_TestCase {
 
-	private $db;
-	
+class TokenizerTest extends TestCommon {
+
 	function __construct() {
+		$this->infile = 'tokenizer_test.csv';
 	}
 
 	function setUp() {
@@ -27,22 +28,6 @@ class TokenizerTest extends PHPUnit_Framework_TestCase {
 		$tokens = jzzf_tokenize('');
 		$this->assertTrue(is_array($tokens));
 		$this->assertEquals(0, count($tokens));
-	}
-
-	function test_all() {
-		$lines = file(dirname(__FILE__) . '/../def/tokenizer_test.csv');
-		if($lines === false) {
-			$this->assertTrue(false, 'Can\t load test definition file');
-		}
-		foreach($lines as $line) {
-			if($line[0] == '#')
-				continue;
-			$arr = str_getcsv($line, '|', "'");
-			if(count($arr)!=3) {
-				$this->assertTrue(false, 'Expected 3 columns in definition file');
-			}
-			$this->check(trim($arr[0]), trim($arr[1]), trim($arr[2]));
-		}
 	}
 
 }
