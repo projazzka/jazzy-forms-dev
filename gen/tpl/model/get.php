119717ff435c79afd03add23723177dc7ce87cb0
Igor Prochazka <igor.prochazka@gmail.com>
2011-12-31 11:43:34 +0100

    Fix getting subitems for get_xxx_by_yyy accessor

--- gen/tpl/model/get.php
+++ gen/tpl/model/get.php
@@ -3,14 +3,14 @@
     $column = $data->column;
     $placeholder = get_placeholder($data->type);
 ?>
-function jzzf_<?=$method?>($id) {
+function jzzf_<?=$method?>($key) {
     global $wpdb;
     $query = "SELECT * FROM {$wpdb->prefix}jzzf_<?=$table?> WHERE `<?=$column?>`=<?=$placeholder?>";
-    $sql = $wpdb->prepare($query, $id);
+    $sql = $wpdb->prepare($query, $key);
     $obj = $wpdb->get_row($sql);
     if($obj) {
 <? foreach($recursion as $id => $child) : ?>
-        $obj-><?=$id?> = jzzf_list_<?=$child?>($id);
+        $obj-><?=$id?> = jzzf_list_<?=$child?>($obj->id);
 <? endforeach ?>
     }
     return $obj;
