7d9b819b99e169390f6a9af3d7c219b319244fe0
Igor Prochazka <igor.prochazka@gmail.com>
2012-01-11 23:25:01 +0100

    Use names instead of IDs at front-end

--- jazzy-forms/front/tmpl-list.php
+++ jazzy-forms/front/tmpl-list.php
@@ -49,8 +49,8 @@ class Jzzf_List_Template {
     }
     
     function number($element) { ?>
-    <label class="jzzf_number_label jzzf_element_label" for="<?php echo htmlspecialchars($id) ?>"><?php echo htmlspecialchars($element->title) ?></label>
-    <input type="text" id="jzzf_<?php echo htmlspecialchars($element->id) ?>" value="<?php echo htmlspecialchars($element->default) ?>">
+    <label class="jzzf_number_label jzzf_element_label" for="jzzf_<?php echo htmlspecialchars($element->name) ?>"><?php echo htmlspecialchars($element->title) ?></label>
+    <input type="text" id="jzzf_<?php echo htmlspecialchars($element->name) ?>" value="<?php echo htmlspecialchars($element->default) ?>">
 <?php
     }
     
@@ -59,7 +59,7 @@ class Jzzf_List_Template {
     <ul class="jzzf_radio">
     <?php $idx = 0; foreach($element->options as $option) { $idx++; ?>
     <li>
-        <input type="radio" name="jzzf_<?php echo htmlspecialchars($element->id) ?>"<?php if($option->default): ?> checked="checked"<?php endif ?>>
+        <input type="radio" name="jzzf_<?php echo htmlspecialchars($element->name) ?>"<?php if($option->default): ?> checked="checked"<?php endif ?>>
         <label class="jzzf_radio_option_label"><?php echo htmlspecialchars($option->title) ?></label>
     </li>
 <?php
@@ -69,7 +69,7 @@ class Jzzf_List_Template {
     
     function dropdown($element) { ?>
     <label class="jzzf_element_label jzzf_dropdown_label"><?php echo htmlspecialchars($element->title) ?></label>
-    <select id="jzzf_<?php echo htmlspecialchars($element->id) ?>">
+    <select id="jzzf_<?php echo htmlspecialchars($element->name) ?>">
     <?php foreach($element->options as $option) : ?>
     <option<?php if($option->default): ?> checked="checked"<?php endif ?>><?php echo htmlspecialchars($option->title) ?></option>
     <?php endforeach ?>
@@ -78,19 +78,19 @@ class Jzzf_List_Template {
     }
 
     function checkbox($element) { ?>
-    <input type="checkbox" id="jzzf_<?php echo htmlspecialchars($element->id) ?>"<?php if($element->default): ?> checked="checked"<?php endif ?>>
-    <label class="jzzf_checkbox_label" for="jzzf_<?php echo htmlspecialchars($element->id) ?>"><?php echo htmlspecialchars($element->title) ?>
+    <input type="checkbox" id="jzzf_<?php echo htmlspecialchars($element->name) ?>"<?php if($element->default): ?> checked="checked"<?php endif ?>>
+    <label class="jzzf_checkbox_label" for="jzzf_<?php echo htmlspecialchars($element->name) ?>"><?php echo htmlspecialchars($element->title) ?>
 <?php
     }
     
     function hidden($element) { ?>
-        <input type="hidden" id="jzzf_<?php echo htmlspecialchars($element->id) ?>" value="<?php echo htmlspecialchars($element->value)?>">
+        <input type="hidden" id="jzzf_<?php echo htmlspecialchars($element->name) ?>" value="<?php echo htmlspecialchars($element->value)?>">
 <?php
     }
     
     function output($element) { ?>
-        <label class="jzzf_element_label jzzf_output_label" for="jzzf_<?php echo htmlspecialchars($id) ?>"><?php echo htmlspecialchars($element->title) ?></label>
-        <input type="text" readonly="readonly" id="jzzf_<?php echo htmlspecialchars($element->id) ?>">
+        <label class="jzzf_element_label jzzf_output_label" for="jzzf_<?php echo htmlspecialchars($name) ?>"><?php echo htmlspecialchars($element->title) ?></label>
+        <input type="text" readonly="readonly" id="jzzf_<?php echo htmlspecialchars($element->name) ?>">
 <?php
     }
 
