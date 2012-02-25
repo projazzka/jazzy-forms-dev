9320cd7482e2c330080afc6cdc26612487638594
Igor Prochazka <igor.prochazka@gmail.com>
2012-02-25 10:44:39 +0100

    Alter resize script for different output sizes

--- artworks/resize.sh
+++ artworks/resize.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-for f in `ls exported | grep \.png`; do
-    echo "Resizing $f"
-    convert exported/$f -filter Catrom -resize 16x16 resized/$f
+for s in `ls exported`; do
+	for f in `ls exported/$s | grep \.png`; do
+	    echo "Resizing $f to $s pixels"
+	    convert exported/$s/$f -filter Catrom -resize $s resized/$f
+	done
 done
+
