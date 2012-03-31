88a7df10739c31c5b68dd1ae4f17230183d1ff14
Igor Prochazka <igor.prochazka@gmail.com>
2012-03-31 12:56:38 +0200

    Implement send email function

--- jazzy-forms/core/Mail.php
+++ jazzy-forms/core/Mail.php
@@ -1,6 +1,8 @@
 <?php
 
 function jzzf_send_email($email) {
-    // todo: implement
-    echo json_encode($email);
+    $message = $email["message"] . "\r\n\r\n";
+    $headers = 'From: '. $email["from"] . "\r\n" .
+    "Content-type: text/html";
+    return wp_mail($email["to"], $email["subject"], $message, $headers);
 }
