5d509ae5e2b8a3d2519f46cc8a6151864f5016f2
Igor Prochazka <igor.prochazka@gmail.com>
2012-04-05 21:31:05 +0200

    Deploy loggin for email function.

--- jazzy-forms/core/Mail.php
+++ jazzy-forms/core/Mail.php
@@ -4,5 +4,16 @@ function jzzf_send_email($email) {
     $message = $email["message"] . "\r\n\r\n";
     $headers = 'From: '. $email["from"] . "\r\n" .
     "Content-type: text/html";
-    return wp_mail($email["to"], $email["subject"], $message, $headers);
+    if(jzzf_log_enabled()) {
+        jzzf_debug("Email headers: $headers");
+        jzzf_debug("Email to: {$email['to']}");
+        jzzf_debug("Email subject: {$email['subject']}");
+    }
+    if(get_option('jzzf_tweak_suppress_email', false)) {
+        jzzf_info('Emails are suppressed. Not sending.');
+        return true;
+    } else {
+        jzzf_info('Sending email');
+        return wp_mail($email["to"], $email["subject"], $message, $headers);
+    }
 }
