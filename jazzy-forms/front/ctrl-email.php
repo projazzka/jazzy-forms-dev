c4ade1ad0c4eebe07fd7bde50dd95e2f73950611
Igor Prochazka <igor.prochazka@gmail.com>
2012-04-06 11:04:53 +0200

    Avoid function name conflict with jzzf_error (log error vs. http error)

--- jazzy-forms/front/ctrl-email.php
+++ jazzy-forms/front/ctrl-email.php
@@ -2,16 +2,19 @@
 
 function jzzf_ctrl_email() {
     if(!($form = $_REQUEST['_form'])) {
-        jzzf_error(403, "Invalid form");
+        jzzf_http_error(403, "Invalid form");
     }
     if(!($email = jzzf_get_email($form))) {
-        jzzf_error(404, "Form Not Found");
+        jzzf_http_error(404, "Form Not Found");
     }
     $replaced = jzzf_apply_email_templates($email);
     jzzf_send_email($replaced);
 }
 
 function jzzf_apply_email_templates($email) {
+    if(jzzf_log_enabled()) {
+        jzzf_debug("Received email placeholders:" . json_encode($_REQUEST));
+    }
     $names = array('to', 'from', 'cc', 'bcc', 'subject', 'message');
     $replaced = array();
     foreach($names as $name) {
@@ -21,7 +24,9 @@ function jzzf_apply_email_templates($email) {
     return $replaced;
 }
 
-function jzzf_error($code, $msg) {
-    header("HTTP/1.0 $code $msg");
+function jzzf_http_error($code, $msg) {
+    $response = "HTTP/1.0 $code $msg";
+    jzzf_error("Http error: $response");
+    header($response);
     exit;
 }
