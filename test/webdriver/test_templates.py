fe3c1c74f293ac1e90c7fb18ea0857d98e2616cc
Igor Prochazka <igor.prochazka@gmail.com>
2012-07-05 21:30:52 +0200

    Add test case to reproduce Bug82

--- test/webdriver/test_templates.py
+++ test/webdriver/test_templates.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 import unittest, time, re
+import string
 
 class Templates(unittest.TestCase):
     def setUp(self):
@@ -14,12 +15,15 @@ class Templates(unittest.TestCase):
 
     def input(self, input, radio):
         driver = self.driver
+        driver.get(self.base_url + "/templates")
+
         driver.find_element_by_id("jzzf_18_a").clear()
         driver.find_element_by_id("jzzf_18_a").send_keys("%s\t" % input)
         driver.find_element_by_id("jzzf_18_b-%d" % radio).click()
 
     def test_basic(self):
         driver = self.driver
+        driver.get(self.base_url + "/templates")
         self.input(1, 1)
 
         self.assertEqual("Result for 1", driver.find_element_by_id("jzzf_18_element").text)
@@ -29,6 +33,7 @@ class Templates(unittest.TestCase):
 
     def test_float(self):
         driver = self.driver
+        driver.get(self.base_url + "/templates")
         self.input(1.1, 1)
                 
         self.assertEqual("Result for 1.1", driver.find_element_by_id("jzzf_18_element").text)
@@ -38,6 +43,7 @@ class Templates(unittest.TestCase):
 
     def test_float2(self):
         driver = self.driver
+        driver.get(self.base_url + "/templates")
         self.input(1.1, 2)
 
         self.assertEqual("Result for 1.1", driver.find_element_by_id("jzzf_18_element").text)
@@ -45,6 +51,17 @@ class Templates(unittest.TestCase):
         self.assertEqual("More info about 2.2", driver.find_element_by_id("jzzf_18_element_2").text)
         self.assertEqual("about 2.2", driver.find_element_by_link_text("about 2.2").text)
         self.assertEqual("Leading Zeros Propagated: 0001.1", driver.find_element_by_id("jzzf_18_propagated").text)
+
+    def test_bug82(self):
+        driver = self.driver
+        self.driver.get(self.base_url + "/bug82")
+
+        self.assertTrue(string.find(driver.find_element_by_id("jzzf_22_element").text, "No placeholders here"))
+        driver.find_element_by_id("jzzf_22_element_1").send_keys("abc\t")
+        self.assertEqual("abc", driver.find_element_by_id("jzzf_22_element_1_1").text)
+        driver.find_element_by_id("jzzf_22_element_1").send_keys("123\t")
+        self.assertEqual("123", driver.find_element_by_id("jzzf_22_element_1_1").text)
+        self.assertTrue(string.find(driver.find_element_by_id("jzzf_22_element").text, "No placeholders here"))
         
     def tearDown(self):
         self.driver.quit()
