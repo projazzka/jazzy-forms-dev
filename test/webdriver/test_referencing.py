8295173f3c9fc4ceaa93d5e10c8a2381d9ce0309
Igor Prochazka <igor.prochazka@gmail.com>
2012-08-03 16:57:12 +0200

    Extend referencing test cases

--- test/webdriver/test_referencing.py
+++ test/webdriver/test_referencing.py
@@ -10,18 +10,44 @@ class Referencing(unittest.TestCase):
         self.driver.implicitly_wait(30)
         self.base_url = "http://blankito.local/workspace/wpbay/wp/"
     
-    def test_normal(self):
+    def test_defaults(self):
         driver = self.driver
         driver.get(self.base_url + "referencing/")
-        self.assertEqual("123.45", driver.find_element_by_id("jzzf_24_b").get_attribute("value"))
-        self.assertEqual("$123.45", driver.find_element_by_id("jzzf_24_c").get_attribute("value"))
-        self.assertEqual("$123.45", driver.find_element_by_id("jzzf_24_d").get_attribute("value"))
+        self.assertEqual("123.4", driver.find_element_by_id("jzzf_24_b").get_attribute("value"))
+        self.assertEqual("123.40", driver.find_element_by_id("jzzf_24_c").get_attribute("value"))
+        self.assertEqual("123.40 total", driver.find_element_by_id("jzzf_24_d").get_attribute("value"))
         self.assertEqual("#REF!", driver.find_element_by_id("jzzf_24_e").get_attribute("value"))
-        
+
         self.assertEqual("You've chosen Apples", driver.find_element_by_id("jzzf_24_o1").get_attribute("value"))
         self.assertEqual("Texas Instruments", driver.find_element_by_id("jzzf_24_o2").get_attribute("value"))
         self.assertEqual("#REF!", driver.find_element_by_id("jzzf_24_o3").get_attribute("value"))
         self.assertEqual("#REF!", driver.find_element_by_id("jzzf_24_o4").get_attribute("value"))
+        
+    def test_formatted_empty(self):
+        driver = self.driver
+        driver.get(self.base_url + "referencing/")
+        
+        input = driver.find_element_by_id("jzzf_24_a")
+        input.clear()
+        
+        self.assertEqual("0", driver.find_element_by_id("jzzf_24_b").get_attribute("value"))
+        self.assertEqual("0.00", driver.find_element_by_id("jzzf_24_c").get_attribute("value"))
+        self.assertEqual("0.00 total", driver.find_element_by_id("jzzf_24_d").get_attribute("value"))
+        self.assertEqual("#REF!", driver.find_element_by_id("jzzf_24_e").get_attribute("value"))
+
+
+    def test_formatted_nonnumeric(self):
+        driver = self.driver
+        driver.get(self.base_url + "referencing/")
+
+        input = driver.find_element_by_id("jzzf_24_a")
+        input.clear()
+        input.send_keys("igor\t")
+        
+        self.assertEqual("igor", driver.find_element_by_id("jzzf_24_b").get_attribute("value"))
+        self.assertEqual("igor", driver.find_element_by_id("jzzf_24_c").get_attribute("value"))
+        self.assertEqual("igor total", driver.find_element_by_id("jzzf_24_d").get_attribute("value"))
+        self.assertEqual("#REF!", driver.find_element_by_id("jzzf_24_e").get_attribute("value"))
     
     def tearDown(self):
         self.driver.quit()
