f361e3ae7fbe0ad30dd94ed3b9845c59bd5cbfe8
Igor Prochazka <igor.prochazka@gmail.com>
2012-12-07 09:12:30 +0100

    backend test: add elements

--- test/webdriver/test_backend.py
+++ test/webdriver/test_backend.py
@@ -37,6 +37,31 @@ class Backend(unittest.TestCase):
         self.assertEqual(len(options), 1)
         self.assertEqual('A new form', options[0].text)
         self.assertEqual('A new form', driver.find_element_by_id("jzzf_title").get_attribute('value'))
+
+    def test_add_elements(self):
+        driver = self.driver
+        driver.find_element_by_css_selector("#toplevel_page_jzzf_forms_top img").click()
+        driver.find_element_by_id("jzzf_new_form_title").clear()
+        driver.find_element_by_id("jzzf_new_form_title").send_keys("Form1")
+        driver.find_element_by_id("jzzf_new_form_add").click()
+        driver.find_element_by_css_selector('li[jzzf_type="n"]').click()
+        driver.find_element_by_css_selector('li[jzzf_type="a"]').click()
+        driver.find_element_by_css_selector('li[jzzf_type="d"]').click()
+        driver.find_element_by_css_selector('li[jzzf_type="r"]').click()
+        driver.find_element_by_css_selector('li[jzzf_type="c"]').click()
+        driver.find_element_by_css_selector('li[jzzf_type="f"]').click()
+        driver.find_element_by_id("jzzf_form_save").click()
+        options = driver.find_elements_by_css_selector("#jzzf_selector option")
+        self.assertEqual(len(options), 1)
+        self.assertEqual('Form1', options[0].text)
+        self.assertEqual('Form1', driver.find_element_by_id("jzzf_title").get_attribute('value'))
+        elements = driver.find_elements_by_css_selector('#jzzf_elements_list input.jzzf_element_type')
+        self.assertEqual('n', elements[0].get_attribute('value'))
+        self.assertEqual('a', elements[1].get_attribute('value'))
+        self.assertEqual('d', elements[2].get_attribute('value'))
+        self.assertEqual('r', elements[3].get_attribute('value'))
+        self.assertEqual('c', elements[4].get_attribute('value'))
+        self.assertEqual('f', elements[5].get_attribute('value'))
     
     def tearDown(self):
         self.driver.quit()
