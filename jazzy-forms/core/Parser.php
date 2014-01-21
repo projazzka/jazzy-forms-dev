b99e0616cb28087d960f9d292e0707659bd51d49
Igor Prochazka <igor.prochazka@gmail.com>
2014-01-21 18:29:25 +0100

    [Bug129] Eliminate recursion from parsing algorithm

--- jazzy-forms/core/Parser.php
+++ jazzy-forms/core/Parser.php
@@ -4,13 +4,13 @@
   
 formula = comparison;
 comparison = concatenation, [("<" | ">" | "<=" | "=>" | "<>"), comparison];
-concatenation = sum, [ "&", concatenation ];
+concatenation = addition, [ "&", concatenation ];
 addition = subtraction, ["+", addition];
 subtraction = product, ["-", difference];
 multiplication = division, ["*", multiplication];
 division = exponentiation, ["/", division];
-exponentiation = negation, ["^", exponentiation];
-negation = ["-"], percentage
+exponentiation = additive_inverse, ["^", exponentiation];
+additive_inverse = ["-"], percentage
 percentage = term ["%"]
 term = number | string | function | identifier | association;
 
@@ -40,63 +40,96 @@ function jzzf_parse_tokens($in) {
 }
 
 class Jzzf_Parser {
+    private $rest;
+    private $rule_stack;
+    private $result_stack;
+    
     public function __construct($tokens) {
         $this->rest = $tokens;
+        $this->execution_stack = array();
+        $this->result_stack = array();
+    }
+    
+    private function push_execution($step, $data=null) {
+        array_push($this->execution_stack, array($step, $data));
+    }
+    
+    private function pop_execution() {
+        return array_pop($this->execution_stack);
     }
 
+    private function push_result($result) {
+        array_push($this->result_stack, $result);
+    }
+    
+    private function pop_result() {
+        return array_pop($this->result_stack);
+    }
+    
     public function output() {
-        $result = $this->comparison();
+        $this->push_execution('operation', 'comparison');
+        while($this->execution_stack) {
+            //$this->dump();
+            list($step, $data) = $this->pop_execution();
+            $this->$step($data);
+        }
+        //$this->dump();
         if($this->rest) {
             throw new Exception('unexpected trailing symbols');
+        }
+        if(!$this->result_stack) {
+            $result = array();
+        } else {
+            $result = $this->pop_result();
+        }
+        if($this->result_stack) {
+            throw new Exception('internal parsing error');            
         }
         return $result;
     }
 
-    private function comparison() {
-        return $this->operation('comparison', array('<=', '>=', '<', '>', '=', '<>'), 'concatenation');
-    }
-    
-    private function concatenation() {
-        return $this->operation('concatenation', array('&'), 'addition');
-    }
-
-    private function addition() {
-        return $this->operation('addition', array('+'), 'subtraction');
-    }
-
-    private function subtraction() {
-        return $this->operation('subtraction', array('-'), 'multiplication');
-    }
-    
-    private function multiplication() {
-        return $this->operation('multiplication', array('*'), 'division');
-    }
-    
-    private function division() {
-        return $this->operation('division', array('/'), 'exponentiation');
-    }
-
-    private function exponentiation() {
-        return $this->operation('exponentiation', '^', 'negation');
-    }
-    
-    private function operation($name, $operators, $subordinate) {
-        $left = $this->$subordinate();
-        if(!$left) {
-            return array();
+    private function operation($rule) {
+        $left_subrules = array(
+            'comparison' => 'concatenation',
+            'concatenation' => 'addition',
+            'addition' => 'subtraction',
+            'subtraction' => 'multiplication',
+            'multiplication' => 'division',
+            'division' => 'exponentiation',
+            'exponentiation' => 'additive_inverse'
+        );
+        $this->push_execution('operation_ahead', $rule);
+        $left_subrule = $left_subrules[$rule];
+        if($left_subrule == 'additive_inverse') {
+            $this->push_execution($left_subrule);
+        } else {
+            $this->push_execution('operation', $left_subrules[$rule]);
         }
-        if($this->ahead($operators)) {
-            $op = $this->consume();
-            $right = $this->$name();
-            if(!$right) {
-                throw new Exception('missing right operand');
-                return array();
-            }
-            return array('o', $op[0], $left, $right);
+    }
+
+    private function operation_ahead($rule) {
+        static $operators = array(
+            'comparison' => array("<", ">", "<=", ">=", "<>", "="),
+            'concatenation' => array("&"),
+            'addition' => array("+"),
+            'subtraction' => array("-"),
+            'multiplication' => array("*"),
+            'division' => array("/"),
+            'exponentiation' => array("^")
+        );
+        if($this->ahead($operators[$rule])) {
+            $operator = $this->consume();
+            $this->push_execution('operation_production', $operator[0]);
+            $this->push_execution('operation', $rule);
         }
-        return $left;
     }
     
+    private function operation_production($operator) {
+        $right = $this->pop_result();
+        $left = $this->pop_result();
+        $this->push_result(array('o', $operator, $left, $right));
+    }
+        
     private function ahead($tokens, $skip=0) {
         if(count($this->rest) <= $skip) {
             return false;
@@ -125,113 +158,133 @@ class Jzzf_Parser {
     }
 
     private function arguments() {
-        $num = 0;
-        $result = array();
-        $first = $this->comparison();
-        if($first) {
-            $result[] = $first;
+        if(!$this->ahead(')')) {
+            $this->push_execution('arguments_production', 1);
+            $this->push_execution('operation', 'comparison');
+        } else {
+            $this->push_execution('arguments_production', 0);            
         }
-        while(true) {
-            if($this->ahead(')')) {
-                break;
+    }
+    
+    private function arguments_production($number) {
+        if($this->ahead(')')) {
+            $this->consume();
+            $arguments = array();
+            for($i=0; $i<$number; $i++) {
+                $argument = $this->pop_result();
+                array_unshift($arguments, $argument);
             }
-            if($this->ahead(',')) {
-                $this->consume();
-                $next = $this->comparison();
-                $result[] = $next;
-                $num++;
-            } else {
-                throw new Exception("Closing bracket or comma expected");
-            }
+            $this->push_result($arguments);            
+        } elseif($this->ahead(',')) {
+            $this->consume();
+            $this->push_execution('arguments_production', $number+1);
+            $this->push_execution('operation', 'comparison');
+        } else {
+            throw new Exception('Closing bracket or comma expected');            
         }
-        $this->consume(); // closing bracket
-        return $result;
     }
 
     private function func() {
         $id = $this->consume();
         $this->consume();
-        $args = $this->arguments();
-        return array_merge(array('f', strtolower($id[1])), $args);
+        $this->push_execution('func_production', strtolower($id[1]));
+        $this->push_execution('arguments', 0);
+    }
+    
+    private function func_production($id) {
+        $args = $this->pop_result();
+        $result = array_merge(array('f', $id), $args);
+        $this->push_result($result);
     }
     
     private function variable() {
         $id = $this->consume();
-        return array('v', strtolower($id[1]));
+        $this->push_result(array('v', strtolower($id[1])));
     }
         
     private function association() {
         $this->consume();
-        $content = $this->comparison();
-        if(!$content) {
-            throw new Exception('Empty bracket term');
-        }
+        
+        $this->push_execution('association_production');
+        $this->push_execution('operation', 'comparison');
+    }
+    
+    private function association_production() {
+        $content = $this->pop_result();
         if(!$this->ahead(')')) {
             throw new Exception('Close bracket expected');
         }
         $this->consume();
-        return $content;
+        $this->push_result($content);
     }
 
-    private function negation() {
+    private function additive_inverse() {
         if($this->ahead('-')) {
             $this->consume();
-            if($this->ahead('n')) {
-                $num = $this->consume();
-                $num[1] *= (-1);
-                return $num;
-            } else {
-                $positive = $this->percentage();
-                    if(!$positive) {
-                    throw new Exception('missing negation operand');
-                    return array();
-                }
-                return array("o", "-", array("n", 0), $positive);
-            }
+            $this->push_execution('additive_inverse_production');
+        }
+        $this->push_execution('percentage');
+    }
+    
+    private function additive_inverse_production() {
+        $operand = $this->pop_result();
+        if(!$operand) {
+            throw new Exception('missing operand for additive inverse');
+        }
+        if($operand[0] == 'n') {
+            $operand[1] *= (-1);
+            $this->push_result($operand);
         } else {
-            return $this->percentage();
+            $this->push_result(array("o", "-", array("n", 0), $operand));
         }
     }
 
     private function percentage() {
-        $value = $this->term();
-        if($this->ahead('%')) {
-            if(!$value) {
-                throw new Exception("Unexpected percentage sign");
-            }
+        $this->push_execution('percentage_production');
+        $this->push_execution('term');
+    }
+
+    private function percentage_production() {
+        $value = $this->pop_result();
+        if($this->ahead(array('%'))) {
             $this->consume();
             if($value[0] == 'n') {
                 $value[1] /= 100;
-                return $value;
+                $this->push_result($value);
             } else {
-                return array("o", "/", $value, array("n", 100));
+                $this->push_result(array("o", "/", $value, array("n", 100)));
             }
+        } else {
+            $this->push_result($value);
         }
-        return $value;
     }
         
     private function term() {
-        //$this->dump();
         if($this->complete()) {
-            return array();
+            $this->push_result(array());
         }
         else if($this->ahead(array('n', 's'))) { // number or string
-            return $this->consume();
+            $this->push_result($this->consume());
         }
         else if($this->ahead('i')) { // identifier
             if($this->ahead('(', 1)) {
-                return $this->func();
+                $this->push_execution('func');
+            } else {
+                $this->push_execution('variable');
             }
-            return $this->variable();
         }
         else if($this->ahead('(')) {
-            return $this->association();
+            $this->push_execution('association');
         }
-        return null;
+        else {
+            $this->push_result(array());
+        }
     }
     
     // for debugging only
     private function dump() {
-        print json_encode($this->rest) . "\n";
+        print "Rest: " . json_encode($this->rest) . "\n";
+        print "Execution stack: " . json_encode($this->execution_stack) . "\n";
+        print "Result stack: " . json_encode($this->result_stack) . "\n";
     }
 }
